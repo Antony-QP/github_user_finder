2b2b86ba721f9e4448732cfcbeff017302ad9200
Antony Parker <antonyqparker@gmail.com>
2020-08-14 15:19:47 +0200

    can search for users and their repos will be displayed dynamically

--- github.js
+++ github.js
@@ -1,16 +1,22 @@
 class GitHub {
   constructor() {
     this.client_id = '48d479ea43794a140f57';
-    this.client_secret = '4d2c3394ad683366e23b6ed112061081743017b2'
+    this.client_secret = '4d2c3394ad683366e23b6ed112061081743017b2';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-    const profile = await profileResponse.json();
+    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+
+    const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
--- ui.js
+++ ui.js
@@ -2,7 +2,7 @@ class UI {
   constructor() {
     this.profile = document.getElementById('profile');
   }
-
+  // Display profile in UI
   showProfile(user) {
     this.profile.innerHTML =
       `<div class="card card-body mb-3">
@@ -27,9 +27,65 @@ class UI {
       </div>
     </div>
     <h3 class="page-heading mb-3">Latest Repos</h3>
-    <div id=""repos"></div>
-    
-    `
+    <div id="repos"></div>     `
+  }
 
+  // Show user repos
+  showRepos(repos) {
+    let output = '';
+
+    repos.forEach(function (repo) {
+      output += `
+      <div class="card card-body mb-2">
+        <div class="row">
+          <div class="col-md-6">
+          <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+          </div>
+          <div class="col-md-6">
+          <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+          <span class="badge badge-secondary">Watchers count: ${repo.watchers}</span>
+          <span class="badge badge-success">Forks count: ${repo.forks_count}</span>
+          </div>
+        </div>
+      </div>
+      `
+    })
+    // Output repos
+    document.getElementById('repos').innerHTML = output;
+  }
+
+  // Show alert message
+  showAlert(message, className) {
+    // Clear any remaining alerts
+    this.clearAlert()
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = className;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.searchContainer');
+    // Get search box
+    const search = document.querySelector('.search');
+    // Insert alert
+    container.insertBefore(div, search);
+
+    // Timeout after three seconds
+    setTimeout(() => {
+      this.clearAlert()
+    }, 3000);
+  }
+
+  // Clear alert message
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+    if (currentAlert) {
+      currentAlert.remove();
+    }
+  }
+  // Clear profile
+  clearProfile() {
+    this.profile.innerHTML = '';
   }
 }
